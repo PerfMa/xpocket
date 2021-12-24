d10926519c2476d932845f4691a8789a3545dcf7
ferdiknight <yin.tong@perfma.com>
2021-12-24 14:59:38 +0800

    fix unneeded version change

--- xpocket-runtime/src/main/java/com/perfma/xlab/xpocket/utils/XPocketConstants.java
+++ xpocket-runtime/src/main/java/com/perfma/xlab/xpocket/utils/XPocketConstants.java
@@ -33,7 +33,7 @@ public class XPocketConstants {
 
     public static final String XPOCKET_COMMAND_PACKAGE = "com.perfma.xlab.xpocket.command.impl";
 
-    public static final String VERSION = "2.1.1-RELEASE";
+    public static final String VERSION = "2.1.2-alpha";
 
     public static final String GITHUB = "https://github.com/PerfMa/xpocket";
 
